b28028436063554787aa2109e155baba577e05cb
smithwithatypo <smithwithatypo@gmail.com>
2024-01-15 00:24:15 -0600

    added delete and add row functions

--- server.js
+++ server.js
@@ -2,60 +2,115 @@ import pg from 'pg';
 import config from './dbconfig.js';
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
-const app = express()
-const port = 3000
-
-app.use(cors());
-
-app.get('/', (req, res) => {
-  res.send({'message': 'Hello World!'})
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const app = express();
+const port = 3000;
 
 
-
-
-
-
+// CONNECT
 const pool = new pg.Pool(config);
 
+
+
+// not needed (delete later)
+// const db = await pool.connect();
+// db.release();
+
+
+
+async function insert(pool, name, number){
+    const text = 'INSERT INTO test_table(name, number) VALUES($1, $2)';
+    const values = [name, number];
+    const res = await pool.query(text, values);
+    return res;
+}
+
+
+async function deleteRow(pool, name){    
+    const text = 'DELETE FROM test_table WHERE name = $1';
+    const values = [name];
+    const res = await pool.query(text, values);
+    return res;
+}
+
+
+
+async function get_all(pool){
+    const res = await pool.query('SELECT * FROM test_table');
+    return res.rows;
+}
+
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', async (req, res) => {
+  const table_results = await get_all(pool); 
+//   console.log(table_results);  // for debugging
+  res.send(table_results);
+})
+
+app.post('/insert', async (req, res) => {
+    // console.log(req.body);  // for debugging
+    let name = req.body.name;
+    let number = req.body.number;
+
+    let db_result = await insert(pool, name, number);
+    console.log("rows added:", db_result["rowCount"]);
+
+    res.send({'message': `added ${name}, ${number} to table`})
+})
+
+app.post('/delete', async (req, res) => {
+    console.log(req.body);
+    let name = req.body.name;
+
+    let db_result = await deleteRow(pool, name);
+    console.log("rows deleted: ", db_result["rowCount"]);
+
+    res.send({'message': `deleted ${name} from table`})
+})
+
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
+
+
+
+
+
+
+// CONNECT
+// const pool = new pg.Pool(config);
+
 // SET VARIABLES TO INSERT INTO TABLE
-let name = 'bird';
-let number = '300';
+// let name = 'bird';
+// let number = '300';
 
 // START CONNECTION
-const client = await pool.connect();
+// const db = await pool.connect();
 
 // INSERT INTO TABLE
 // const text = 'INSERT INTO test_table(name, number) VALUES($1, $2)';
 // const values = [name, number];
-// const res = await client.query(text, values);
+// const res = await db.query(text, values);
 // console.log(res);
 
 // DELETE FROM TABLE
 // const text = 'DELETE FROM test_table WHERE name = $1';
 // const values = [name];
-// const res = await client.query(text, values);
+// const res = await db.query(text, values);
 // console.log(res);
 
 // RETURN ALL ROWS
-const res = await client.query('SELECT * FROM test_table');
-console.log(res.rows);
+// const res = await db.query('SELECT * FROM test_table');
+// console.log(res.rows);
 
 // RELEASE CONNECTION
-client.release();
+// db.release();
 
-// PRINT ROWS AND DECONSTRUCT OBJECTS
-// function print_items(){
-//     let res_name = res.rows[0]['name'];
-//     let res_number = res.rows[0]['number'];
-//     console.log(typeof(res_name));
-//     console.log(typeof(res_number))
-// }
-// print_items();
 
 
